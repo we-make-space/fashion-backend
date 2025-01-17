ac4439ddbb335f645476ef76e364611cddb4499f
Bubu Adico⚡️ <adikobubu@gmail.com>
2025-01-17 12:47:21 +0000

    refactor(modify the getAllOrders): modify the getAllOrders

--- Server/src/api/v1/controllers/orderController.js
+++ Server/src/api/v1/controllers/orderController.js
@@ -1,12 +1,13 @@
 import asyncHandler from "express-async-handler";
 import { prisma } from "../config/prismaConfig.js";
 
+//* Create order for products
 export const createOrder = asyncHandler(async (req, res) => {
 	const { userId, status, totalAmount, orderItems, addressId } = req.body;
 
 	console.log("Received request to create order", orderItems);
 	try {
-	 
+	
 		const order = await prisma.order.create({
 			data: {
 				user: {
@@ -15,22 +16,22 @@ export const createOrder = asyncHandler(async (req, res) => {
 				status: status || "PENDING",
 				totalAmount,
 items: {
-  createMany: {
+createMany: {
 	data: await Promise.all(orderItems.map(async item => {
-	  const product = await prisma.product.findUnique({
+	const product = await prisma.product.findUnique({
 		where: { id: item.productId },
 		select: { price: true },
-	  });
-	  if (!product) {
+	});
+	if (!product) {
 		throw new Error(`Product with id ${item.productId} not found`);
-	  }
-	  return {
+	}
+	return {
 		productId: item.productId,
 		quantity: item.quantity,
 		price: product.price,
-	  };
+	};
 	})),
-  },
+},
 },
 				Shipping: {
 					create: {
@@ -59,12 +60,11 @@ items: {
 //* Get all orders with pagination
 export const getAllOrders = asyncHandler(async (req, res) => {
 	try {
-	// Extract page and limit from query parameters, set defaults if not provided
-	const page = parseInt(req.query.page, 10) || 1; // Default to page 1
-	const limit = parseInt(req.query.limit, 10) || 10; // Default to 10 items per page
+	const page = parseInt(req.query.page, 10) || 1; 
+	const limit = parseInt(req.query.limit, 10) || 10; 
 	const skip = (page - 1) * limit;
 
-	// Fetch paginated orders
+	//* Fetch paginated orders
 	const orders = await prisma.order.findMany({
 		skip,
 		take: limit,
@@ -97,23 +97,36 @@ export const getAllOrders = asyncHandler(async (req, res) => {
 			lastName: true,
 			},
 		},
+		Shipping: {
+			select: {
+			carrier: true,
+			trackingNumber: true,
+			address: {
+				select: {
+				id: true,
+				street: true,
+				city: true,
+				town: true,
+				Region: true,
+				country: true,
+				},
+			},
+			},
+		},
 		},
 	});
 
-	// Fetch total number of orders for pagination metadata
 	const totalOrders = await prisma.order.count();
 
-	// Calculate total pages
 	const totalPages = Math.ceil(totalOrders / limit);
 
-	// Return response with pagination metadata
 	res.status(200).json({
-		data: orders,
 		meta: {
-		totalOrders,
-		currentPage: page,
-		totalPages,
+			totalOrders,
+			currentPage: page,
+			totalPages,
 		},
+		data: orders,
 	});
 	} catch (error) {
 	console.error("Error fetching orders:", error);
