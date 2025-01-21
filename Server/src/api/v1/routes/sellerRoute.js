d7317de8081d8596be37e9d121ed03cc48071113
Bubu Adico⚡️ <adikobubu@gmail.com>
2025-01-21 18:23:25 +0000

    fix(tab and spaces): fixed tabs and spaces

--- Server/src/api/v1/routes/sellerRoute.js
+++ Server/src/api/v1/routes/sellerRoute.js
@@ -1,33 +1,39 @@
-import express from "express";
+import express from 'express';
 // import { logAction, logError } from "../../../middlewares/loggerMiddlewares.js";
-import { getAllSeller, getAllSellerOrders, getAllSellerProducts, getSellerOrderById } from "../controllers/sellerController.js";
-
+import {
+	getAllSeller,
+	getAllSellerOrders,
+	getAllSellerProducts,
+	getSellerOrderById
+} from '../controllers/sellerController.js';
 
 const router = express.Router();
 
-router.get("/all", 
-	// logAction("Fetching all sellers"), 
-	getAllSeller);
+router.get(
+	'/all',
+	// logAction("Fetching all sellers"),
+	getAllSeller
+);
 
 router.get(
-	"/sellerProducts/:email",
+	'/sellerProducts/:email',
 	// logAction("Fetching all seller products by email"),
-	getAllSellerProducts,
+	getAllSellerProducts
 	// logError
 );
 
-router.get("/SellerOrders/:email", 
-	// logAction("Fetching all seller orders"), 
-	getAllSellerOrders,	
+router.get(
+	'/SellerOrders/:email',
+	// logAction("Fetching all seller orders"),
+	getAllSellerOrders
 	// logError
 );
 
-router.get("/sellerOrderById/:id", 
-	// logAction("Fetching all seller orders by id"), 
-	getSellerOrderById,	
+router.get(
+	'/sellerOrderById/:id',
+	// logAction("Fetching all seller orders by id"),
+	getSellerOrderById
 	// logError
 );
-
-
 
 export { router as sellerRoute };
