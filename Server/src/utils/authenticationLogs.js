e4f88351cb8bd46e699bbcbdec2eea0ea43d728c
Bubu Adico⚡️ <adikobubu@gmail.com>
2025-02-02 21:18:06 +0000

    refactor(remove vast files)

--- Server/src/utils/authenticationLogs.js
+++
@@ -1,9 +0,0 @@
-import winston from 'winston';
-
-const authenticationLogger = winston.createLogger({
-	level: 'info',
-	format: winston.format.json(),
-	transports: [new winston.transports.File({ filename: path.join('logs', 'auth-failures.log') })]
-});
-
-export default authenticationLogger;
