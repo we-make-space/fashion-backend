a5d6df9af8bb59b430661cde486a37601342523d
Bubu Adico⚡️ <adikobubu@gmail.com>
2025-02-02 21:20:35 +0000

    pull changes

--- Server/src/middlewares/protect.js
+++
@@ -1,9 +0,0 @@
-const protect = (req, res, next) => {
-    if(req.oidc.isAuthenticated()) {
-        next();
-    } else {
-        res.status(401).json({ message: "Unauthorized" });
-    }
-}
-
-export default protect;
